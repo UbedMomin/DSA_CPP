4f372c14ff5deb06595fae2dea6efef39d8c3219
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-09-01 20:07:50 +0530

    updated upto floyds trinagle pattern

--- lect4.cpp
+++ lect4.cpp
@@ -132,22 +132,79 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n = 5;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         char ch = 'A' + i;
+//         for (int j = 0; j <= i; j++)
+//         {
+//             cout << ch << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n = 5;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 1; j <= i + 1; j++)
+//         {
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n = 4;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = i + 1; j > 0; j--)
+//         {
+//             cout << j<<" ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+// FLOYD'S TRIANGLE PATTERN
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int n = 5;
+    int n = 4;
 
+    int num = 1;
     for (int i = 0; i < n; i++)
     {
-        char ch = 'A' + i;
-        for (int j = 0; j <= i; j++)
+        for (int j = i + 1; j > 0; j--)// backwords => i+1
         {
-            cout << ch << " ";
+            cout << num<< " ";
+            num++;
         }
         cout << endl;
     }
+
     return 0;
 }
-
