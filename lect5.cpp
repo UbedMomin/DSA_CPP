17bd572eb9ea0f3f4191f600aa7359c7ef511964
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-10-09 22:52:42 +0530

    day 9/10/25

--- lect5.cpp
+++ lect5.cpp
@@ -4,15 +4,38 @@
 using namespace std;
 
 // function defination
-int printHello()
+// int printHello()
+// {
+//     cout << "hello\n";
+//     return 3;
+// }
+
+int sumN(int n)
 {
-    cout << "hello\n";
-    return 3;
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int factorialN(int n)
+{
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+    return fact;
 }
 
 int main()
 {
-    cout << printHello() << endl; // function call
+    // cout << printHello() << endl; // function call
     // cout << "value = " << val << endl;
+
+    cout << factorialN(4) << endl;
+    cout << factorialN(5) << endl;
     return 0;
 }
