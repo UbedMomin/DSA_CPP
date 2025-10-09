68f81502c460eae3c1f7b5c9c6c20e95e02fd014
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-10-09 11:26:09 +0530

    funtion Lect 5 simple function call

---
+++ lect5.cpp
@@ -0,0 +1,18 @@
+// Functions
+
+#include <iostream>
+using namespace std;
+
+// function defination
+int printHello()
+{
+    cout << "hello\n";
+    return 3;
+}
+
+int main()
+{
+    printHello();
+
+    return 0;
+}
