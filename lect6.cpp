970c3bd0b60d3a5a7318dddd3f662b8cc56a06f7
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-10-20 11:37:55 +0530

    lect 6 binary number system

---
+++ lect6.cpp
@@ -0,0 +1,76 @@
+// g++ lect6.cpp -o lect6
+// ./lect6
+
+// Decimal to Binary Conversion
+
+// #include <iostream>
+// using namespace std;
+
+// int decToBinary(int decNum)
+// {
+//     int ans = 0, pow = 1;
+
+//     while (decNum > 0)
+//     {
+//         int rem = decNum % 2;
+//         decNum /= 2;
+
+//         ans += (rem * pow);
+//         pow *= 10;
+//     }
+//     return ans; // binary form
+// }
+
+// int main()
+// {
+
+//     int decNum = 50;
+//     // cout << decToBinary(decNum) << endl;
+
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         cout << decToBinary(i) << endl;
+//     }
+//     return 0;
+// }
+
+// Binary to Decimal Conversion
+#include <iostream>
+using namespace std;
+
+int decToBinary(int decNum)
+{
+    int ans = 0, pow = 1;
+
+    while (decNum > 0)
+    {
+        int rem = decNum % 2;
+        decNum /= 2;
+
+        ans += (rem * pow);
+        pow *= 10;
+    }
+    return ans; // binary form
+}
+
+int binToDec(int binNum)
+{
+    int ans = 0, pow = 1;
+
+    while (binNum > 0)
+    {
+        int rem = binNum % 10;
+        ans += (rem * pow);
+
+        binNum /= 10;
+        pow *= 2;
+    }
+    return ans; // decimal form
+}
+
+int main()
+{
+
+    cout << binToDec(1010) << endl;
+    return 0;
+}
