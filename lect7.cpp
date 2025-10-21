3ab4c4a09ba6d6cea1f23617508f4f76e23aaabc
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-10-21 15:26:14 +0530

    lect 7  bitwise operators and data types

---
+++ lect7.cpp
@@ -0,0 +1,96 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a = 4, b = 8;
+
+//     cout << (a & b) << endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a = 4, b = 8;
+
+//     cout << (a | b) << endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a = 4, b = 8;
+
+//     cout << (a ^ b) << endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << (10 << 2) << endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << (8 >> 2) << endl;
+//     return 0;
+// }
+
+//HW 
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << (6 & 10) << endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << (6 | 10) << endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << (6 ^ 10) << endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << (10 << 2) << endl;
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << (10 >> 1) << endl;
+    return 0;
+}
